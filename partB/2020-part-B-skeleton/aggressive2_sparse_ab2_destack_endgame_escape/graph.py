101c29a208e8209fef7928949f9e977002a85fb2
rickygeng <rickygeng97@gmail.com>
2020-05-11 12:22:58 +0800

    stopper

---
+++ partB/2020-part-B-skeleton/aggressive2_sparse_ab2_destack_endgame_escape/graph.py
@@ -0,0 +1,266 @@
+from collections import deque
+import random
+from aggressive2_sparse_ab2_destack_endgame_escape.square import *
+from aggressive2_sparse_ab2_destack_endgame_escape.util import *
+import copy
+
+
+def generate_adjacency_list(layout, adjacency_func, our_colour):
+    """Return a dictionary of adjacency list
+    :param squares: list of square coordinates
+    :type squares: list
+    """
+
+    our_squares = layout[our_colour]
+    adjacency_list = {}
+    for current_square in our_squares:
+        adjacency_list[tuple(current_square)] = []
+        for other_square in our_squares:
+            if other_square != current_square and other_square in adjacency_func(1, current_square,
+                                                                                 layout, our_colour):
+                adjacency_list[tuple(current_square)].append(other_square)
+
+    return adjacency_list
+
+
+def bfs_shortest_path(blacks, graph, start, end):
+    """Return shortest path from start to end in form of a list of coordinates
+    :param graph: dictionary of adjacency list
+    :param start: coordinate (x, y)
+    :param end: coordinate (x, y)
+    """
+
+    # https://stackoverflow.com/questions/8922060/how-to-trace-the-path-in-a-breadth-first-search
+    # maintain a queue of paths
+    queue = []
+    # push the first path into the queue
+    queue.append([start])
+
+    visited = []
+
+    while queue:
+        # get the first path from the queue
+        path = queue.pop(0)
+        # get the last node from the path
+        node = path[-1]
+        if node not in visited:
+            visited.append(node)
+        # path found
+        if node == end:
+            return path
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+
+        for adjacent in graph.get(node, []):
+            inbetween = []
+
+            if adjacent[1] == node[1]:
+                if adjacent[2] < node[2]:
+                    for i in range(node[2] - adjacent[2] - 1):
+                        inbetween.append((adjacent[1], adjacent[2] + i + 1))
+                else:
+                    for i in range(adjacent[2] - node[2] - 1):
+                        inbetween.append((adjacent[1], node[2] + i + 1))
+
+            else:
+                if adjacent[1] < node[1]:
+                    for i in range(node[1] - adjacent[1] - 1):
+                        inbetween.append((adjacent[2], adjacent[1] + i + 1))
+                else:
+                    for i in range(adjacent[1] - node[1] - 1):
+                        inbetween.append((adjacent[2], node[1] + i + 1))
+
+            def helper():
+                if inbetween == []:
+                    return True
+
+                for token in inbetween:
+                    if token in [tuple(token[1:]) for token in blacks]:
+                        return True
+                return False
+
+            if helper():
+                new_path = list(path)
+                if adjacent in new_path:
+                    continue
+                new_path.append(adjacent)
+                if sorted(set(new_path), key=lambda k: (k[0], k[1])) == sorted(visited):
+                    continue
+                queue.append(new_path)
+
+
+def dfs(graph, start):
+    # https://github.com/TheAlgorithms/Python/blob/9eac17a4083ad08c4bb0520cb0b8e5ce385f9ce0/graphs/dfs.py
+    explored, stack = set(), [start]
+    while stack:
+        v = (
+            stack.pop()
+        )  # one difference from BFS is to pop last element here instead of first one
+        if v in explored:
+            continue
+
+        explored.add(v)
+
+        for w in graph[v]:
+            if w not in explored:
+                stack.append(w)
+    return explored
+
+
+def bfs(graph, start):
+    # https://pythoninwonderland.wordpress.com/2017/03/18/how-to-implement-breadth-first-search-in-python/
+    # keep track of all visited nodes
+    explored = []
+    # keep track of nodes to be checked
+    queue = [start]
+
+    # keep looping until there are nodes still to be checked
+    while queue:
+        # pop shallowest node (first node) from queue
+        node = queue.pop(0)
+        if node not in explored:
+            # add node to list of checked nodes
+            explored.append(node)
+            neighbours = graph[node]
+
+            # add neighbours of node to queue
+            for neighbour in neighbours:
+                queue.append(neighbour)
+    return explored
+
+
+def connected_components(graph):
+    """Return a list of connected black tokens. e.g. [[(0, 2), (1, 1), (2, 0)], [(4, 7)], [(7, 7)]]
+    """
+
+    # https://stackoverflow.com/questions/10301000/python-connected-components
+    seen = set()
+
+    for root in graph.keys():
+        if root not in seen:
+            seen.add(root)
+            component = []
+            queue = deque([root])
+            while queue:
+                node = queue.popleft()
+                component.append(node)
+                for neighbor in graph[node]:
+                    nei = tuple(neighbor)
+                    if nei not in seen:
+                        seen.add(nei)
+                        queue.append(nei)
+            yield component
+
+
+def minimax(layout, exp, depth, alpha, beta, maximizing_player, colour, our_colour, escape):
+    colour = "blacks" if colour == "whites" else "whites"
+
+    opponent_colour = "blacks" if our_colour == "whites" else "whites"
+
+    if sum(t[0] for t in layout[opponent_colour]) == 0:
+        return 100, None, escape
+
+    if depth == 0 or sum(t[0] for t in layout[our_colour]) == 0:
+        if our_colour == "whites":
+            # return sum(t[0] for t in layout["whites"]) - sum(t[0] for t in layout["blacks"]), None, escape
+            return sum(t[0] for t in exp["blacks"]) - sum(t[0] for t in exp["whites"]), None, escape
+
+        elif our_colour == "blacks":
+            # return sum(t[0] for t in layout["blacks"]) - sum(t[0] for t in layout["whites"]), None, escape
+            return sum(t[0] for t in exp["whites"]) - sum(t[0] for t in exp["blacks"]), None, escape
+
+    if maximizing_player:
+        max_eval = -13
+        max_action = None
+        action_layout_list = generate_all_layouts(layout, exp, colour)
+        for t, a, l, exp in action_layout_list:
+            eval, _, escape = minimax(l, exp, depth - 1, alpha, beta, False, colour, our_colour, escape)
+            if depth == 3 and a[0] == "MOVE" and eval < 0:
+                escape = True
+
+            if eval > max_eval:
+                max_eval = eval
+                max_action = a
+
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return max_eval, max_action, escape
+    else:
+        min_eval = 13
+        min_action = None
+        action_layout_list = generate_all_layouts(layout, colour)
+        for t, a, l, exp in action_layout_list:
+            eval, _, escape = minimax(l, exp,  depth - 1, alpha, beta, True, colour, our_colour, escape)
+            print(a, exp, eval)
+
+            if eval < min_eval:
+                min_eval = eval
+                min_action = a
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return min_eval, min_action, escape
+
+
+def generate_all_layouts(layout, exp, colour):
+    action_layout_list = []
+
+    for token in layout[colour]:
+        destinations = find_adjacent_squares(token, layout, colour)
+        for destination in destinations:
+            for numToken in range(token[0]):
+                n = numToken + 1
+                xa, ya = token[1], token[2]
+                xb, yb = destination[1], destination[2]
+                if xa == xb and ya == yb:
+                    action = ("BOOM", (xa, ya))
+                else:
+                    action = ("MOVE", n, (xa, ya), (xb, yb))
+                layout_copy = copy.deepcopy(layout)
+                next_layout, exploded_token_dict = update_layout(action, layout_copy, colour)
+                action_layout_list.append((token, action, next_layout, exploded_token_dict))
+
+    return action_layout_list
+
+
+def update_layout(action, layout, colour):
+    # Update board layout
+    if action[0] == "MOVE":
+
+        exploded_token_dict = {"whites":[],"blacks":[]}
+
+        colour_tokens = layout[colour]
+
+        # Remove n tokens from starting stack
+        n, start, end = action[1], action[2], action[3]
+        for i in range(len(colour_tokens)):
+            token = colour_tokens[i]
+            if token[1] == start[0] and token[2] == start[1]:
+                token[0] -= n
+                break
+
+        # Remove token with 0
+        layout[colour] = [token for token in colour_tokens if token[0] != 0]
+
+        # Add n tokens to ending stack
+        contained = False
+        n, start, end = action[1], action[2], action[3]
+        for j in range(len(colour_tokens)):
+            token = colour_tokens[j]
+            if token[1] == end[0] and token[2] == end[1]:
+                token[0] += n
+                contained = True
+                break
+
+        if contained == False:
+            layout[colour].append([n, end[0], end[1]])
+    elif action[0] == "BOOM":
+        coord = action[1]
+        exploded_token_dict = get_exploded_dict(coord, layout)
+
+        layout["whites"] = [white for white in layout["whites"] if
+                            white not in exploded_token_dict["whites"]]
+        layout["blacks"] = [black for black in layout["blacks"] if
+                            black not in exploded_token_dict["blacks"]]
+
+    return layout, exploded_token_dict
---
+++ partB/2020-part-B-skeleton/aggressive3_sparse_ab_destack_endgame_escape/player.py
@@ -0,0 +1,192 @@
+import random
+from aggressive3_sparse_ab_destack_endgame_escape.square import *
+from aggressive3_sparse_ab_destack_endgame_escape.graph import *
+
+
+class ExamplePlayer:
+    def __init__(self, colour):
+        """
+        This method is called once at the beginning of the game to initialise
+        your player. You should use this opportunity to set up your own internal
+        representation of the game state, and any other information about the 
+        game state you would like to maintain for the duration of the game.
+
+        The parameter colour will be a string representing the player your 
+        program will play as (White or Black). The value will be one of the 
+        strings "white" or "black" correspondingly.
+        """
+        # TODO: Set up state representation.
+
+        self.colour = colour
+        # Initial layout
+        self.layout = {
+            "whites": [[1, 0, 1], [1, 1, 1], [1, 3, 1], [1, 4, 1], [1, 6, 1], [1, 7, 1],
+                       [1, 0, 0], [1, 1, 0], [1, 3, 0], [1, 4, 0], [1, 6, 0], [1, 7, 0]],
+            "blacks": [[1, 0, 7], [1, 1, 7], [1, 3, 7], [1, 4, 7], [1, 6, 7], [1, 7, 7],
+                       [1, 0, 6], [1, 1, 6], [1, 3, 6], [1, 4, 6], [1, 6, 6], [1, 7, 6]]
+        }
+
+        self.old_eval = 0
+
+    def action(self):
+        """
+        This method is called at the beginning of each of your turns to request 
+        a choice of action from your program.
+
+        Based on the current state of the game, your player should select and 
+        return an allowed action to play on this turn. The action must be
+        represented based on the spec's instructions for representing actions.
+        """
+
+        # TODO: Decide what action to take, and return it
+
+        # def find_nearby_opponents():
+        #     nearby = {"whites": [], "blacks": []}
+        #     for our in self.layout[our_colour]:
+        #         surroundings = find_nxn_surrounding_squares(our[1:], 1)
+        #         for surrounding in surroundings:
+        #             for opponent in self.layout[opponent_colour]:
+        #                 if opponent[1] == surrounding[0] and opponent[2] == surrounding[1]:
+        #                     nearby[our_colour].append(our)
+        #                     nearby[opponent_colour].append(opponent)
+        #     return nearby
+
+        def find_nearby_opponents(opponent_colour):
+            nearby = {"whites": [], "blacks": []}
+            for opp in self.layout[opponent_colour]:
+                surroundings = find_nxn_surrounding_squares(opp[1:], opp[0] + 1)
+                for surrounding in surroundings:
+                    for our in self.layout[our_colour]:
+                        if our[1] == surrounding[0] and our[2] == surrounding[1]:
+                            nearby[our_colour].append(our)
+                            nearby[opponent_colour].append(opp)
+            return nearby
+
+        # def find_sparse_destination():
+        #     destinations = find_adjacent_squares(token, self.layout, our_colour)
+        #     destinations = [d for d in destinations if not (xa == d[1] and ya == d[2])]
+        #     min_destination = None
+        #     min_exploded_num = 13
+        #
+        #     # Find a destination that causes least explosion of our token and most explosion of opponent's token
+        #     for destination in destinations:
+        #         coord = destination[1:]
+        #         layout = copy.deepcopy(self.layout)
+        #         layout[our_colour].remove(token)
+        #
+        #         exist = False
+        #         for our in layout[our_colour]:
+        #             if our[1] == destination[1] and our[2] == destination[2]:
+        #                 our[0] += token[0]
+        #                 exist = True
+        #                 break
+        #
+        #         if not exist:
+        #             layout[our_colour].append([token[0], destination[1], destination[2]])
+        #         exploded_token_dict = get_exploded_dict(coord, layout)
+        #         our_exploded, opponent_exploded = exploded_token_dict[our_colour], exploded_token_dict[opponent_colour]
+        #         if sum(t[0] for t in our_exploded) - sum(t[0] for t in opponent_exploded) < min_exploded_num:
+        #             min_exploded_num = sum(t[0] for t in our_exploded) - sum(t[0] for t in opponent_exploded)
+        #             min_destination = destination
+        #     return min_destination
+
+        def find_best_action(layout):
+            action_layout_list = generate_all_layouts(layout, our_colour)
+
+            max_action = None
+            max_cc = 0
+            max_sparse = -1
+            min_dist = 17
+            for t, a, l in action_layout_list:
+
+                # adjacency_list = generate_adjacency_list(l, find_3x3_adjacent_squares, our_colour)
+                #
+                # ccs = list(connected_components(adjacency_list))
+                #
+                # if len(ccs) > max_cc:
+                #     max_cc = len(ccs)
+                #     max_action = a
+                # elif len(ccs) == max_cc and len(l[our_colour]) > max_sparse:
+                #     max_sparse = len(l[our_colour])
+                #     max_action = a
+                # elif len(ccs) == max_cc and len(l[our_colour]) == max_sparse:
+                closestOpponent = None
+                closestOpponentDist = 17
+                for w in layout[opponent_colour]:
+                    dist = abs(t[1] - w[1]) + abs(t[2] - w[2])
+                    if dist < closestOpponentDist:
+                        closestOpponentDist = dist
+                        closestOpponent = w
+
+                if a[0] == "MOVE":
+                    new_dist = abs(a[3][0] - closestOpponent[1]) + abs(a[3][1] - closestOpponent[2])
+                    old_dist = abs(a[2][0] - closestOpponent[1]) + abs(a[2][1] - closestOpponent[2])
+
+                    if new_dist < old_dist and new_dist < min_dist:
+                        min_dist = new_dist
+                        max_action = a
+
+            return max_action
+
+        our_colour = self.colour + "s"
+        opponent_colour = "blacks" if self.colour == "white" else "whites"
+
+        nearby = find_nearby_opponents(opponent_colour)
+        action = None
+
+        if not nearby[opponent_colour]:
+            # Make sparse
+            # token = random.choice(self.layout[our_colour])
+            # n, xa, ya = token[0], token[1], token[2]
+
+            # destination = find_sparse_destination()
+
+            action = find_best_action(self.layout)
+
+        else:
+            # Minimax
+            eval, action, escape = minimax(self.layout, 3, -13, 13, True, opponent_colour, our_colour, False)
+            if eval > self.old_eval or action[0] == "BOOM":
+                self.old_eval = eval
+                return action
+
+            diff = sum(t[0] for t in self.layout[our_colour]) - sum(t[0] for t in self.layout[opponent_colour])
+            # if eval == diff and escape == False:
+            #     # new_l = copy.deepcopy(self.layout)
+            #     # new_l["whites"] = [white for white in new_l["whites"] if
+            #     #                     white not in nearby["whites"]]
+            #     # new_l["blacks"] = [black for black in new_l["blacks"] if
+            #     #                     black not in nearby["blacks"]]
+            #
+            #     if not nearby[our_colour]:
+            #         action = find_best_action(self.layout)
+            #     else:
+            #         for our in nearby[our_colour]:
+            #             action = ("BOOM", (our[1], our[2]))
+            #             break
+            # elif eval > diff and escape == False:
+            #     action = find_best_action(self.layout)
+
+        return action
+
+    def update(self, colour, action):
+        """
+        This method is called at the end of every turn (including your player’s 
+        turns) to inform your player about the most recent action. You should 
+        use this opportunity to maintain your internal representation of the 
+        game state and any other information about the game you are storing.
+
+        The parameter colour will be a string representing the player whose turn
+        it is (White or Black). The value will be one of the strings "white" or
+        "black" correspondingly.
+
+        The parameter action is a representation of the most recent action
+        conforming to the spec's instructions for representing actions.
+
+        You may assume that action will always correspond to an allowed action 
+        for the player colour (your method does not need to validate the action
+        against the game rules).
+        """
+        # TODO: Update state representation in response to action.
+
+        self.layout = update_layout(action, self.layout, colour + 's')
